67bdfc5cafe822918da6c8d1d08a904063cfdc8b
danilorlovskiy-urich <danylo.orlovskiy@urich.org>
2021-09-22 12:32:22 +0300

    (feature) add modal block

--- src/pages/ilo/index.tsx
+++ src/pages/ilo/index.tsx
@@ -4,10 +4,15 @@ import DataContainer from '@components/DataContainer';
 import { CompanyLogoData } from '@components/CompanyLogo/mocked';
 import { DataContainerData } from '@components/DataContainer/mocked';
 
+import AccountDetails from '@/components/AccountDetails';
+import ConnectWallet from '@/components/ConnectWallet';
+
 export default function Ilo() {
   return (
     <div>
       <p>TEST</p>
+      <AccountDetails />
+      <ConnectWallet />
       {CompanyLogoData.map(({ title, img, status }) => {
         return <CompanyLogo title={title} img={img} status={status} />;
       })}
