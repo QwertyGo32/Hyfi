4dfe36a66cc1b4a95b3d8842e13fdd32e517d00a
danilorlovskiy-urich <danylo.orlovskiy@urich.org>
2021-09-24 14:05:19 +0300

    (ilo) creata validation

--- src/components/Btn/styled.tsx
+++ src/components/Btn/styled.tsx
@@ -6,7 +6,6 @@ export enum gradientBtnTypes {
 }
 export const BtnStyled = styled(Button)`
   padding: 14px 22px;
-  min-width: 160px;
   font: normal normal medium 15px/70px Montserrat;
   
   &.home-page_btn {
