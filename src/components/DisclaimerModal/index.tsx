6dfdbb7d0540e4be20076c67e60aaf404d9efc8a
danilorlovskiy-urich <danylo.orlovskiy@urich.org>
2021-09-27 16:54:55 +0300

    (modal) create lodare

--- src/components/DisclaimerModal/index.tsx
+++ src/components/DisclaimerModal/index.tsx
@@ -83,11 +83,6 @@ export default function DisclaimerModal({ ...props }) {
               <Col>
                 <InputGroup className="mb-3">
                   <InputGroup.Checkbox aria-label="Checkbox for following text input" />
-                  <FormControl aria-label="Text input with checkbox" />
-                </InputGroup>
-                <InputGroup>
-                  <InputGroup.Radio aria-label="Radio button for following text input" />
-                  <FormControl aria-label="Text input with radio button" />
                 </InputGroup>
               </Col>
             </Row>
